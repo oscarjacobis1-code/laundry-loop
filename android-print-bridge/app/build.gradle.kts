acc0b9f6652a334a7fbb9b9c1f640e460b2baaba
oscarjacobis1-code <oscarjacobis1@gmail.com>
2026-09-13 12:08:35 -0400

    Bump printer app for receipt styling update

--- android-print-bridge/app/build.gradle.kts
+++ android-print-bridge/app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.laundryloop.printbridge"
         minSdk = 26
         targetSdk = 35
-        versionCode = 10
-        versionName = "1.9.0"
+        versionCode = 11
+        versionName = "1.10.0"
     }
 
     if (!releaseKeystorePath.isNullOrBlank()) {
