68a68342f6376d59ea00516821f9265f440c4a8b
oscarjacobis1-code <oscarjacobis1@gmail.com>
2026-09-13 10:09:46 -0400

    Bump printer app after receipt media fix

--- android-print-bridge/app/build.gradle.kts
+++ android-print-bridge/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.laundryloop.printbridge"
         minSdk = 26
         targetSdk = 35
-        versionCode = 6
-        versionName = "1.5.0"
+        versionCode = 7
+        versionName = "1.6.0"
     }
 
     compileOptions {
