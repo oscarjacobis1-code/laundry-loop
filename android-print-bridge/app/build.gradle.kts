6c9e6411d72703a25faee3cd6801ae394dc23f8e
oscarjacobis1-code <oscarjacobis1@gmail.com>
2026-09-13 10:45:43 -0400

    Bump printer app for direct ESC/POS receipt path

--- android-print-bridge/app/build.gradle.kts
+++ android-print-bridge/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.laundryloop.printbridge"
         minSdk = 26
         targetSdk = 35
-        versionCode = 8
-        versionName = "1.7.0"
+        versionCode = 9
+        versionName = "1.8.0"
     }
 
     compileOptions {
