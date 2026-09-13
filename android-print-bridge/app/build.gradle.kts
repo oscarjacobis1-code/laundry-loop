727eba46011c50a690cb9cbfa40ae0f200dc702d
oscarjacobis1-code <oscarjacobis1@gmail.com>
2026-09-13 13:05:32 -0400

    Bump printer app for total weight display

--- android-print-bridge/app/build.gradle.kts
+++ android-print-bridge/app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.laundryloop.printbridge"
         minSdk = 26
         targetSdk = 35
-        versionCode = 13
-        versionName = "1.12.0"
+        versionCode = 14
+        versionName = "1.13.0"
     }
 
     if (!releaseKeystorePath.isNullOrBlank()) {
