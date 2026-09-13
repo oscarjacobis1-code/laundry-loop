ed97c800b3e957bc4cd87b5150f5fc0f4c45e633
oscarjacobis1-code <oscarjacobis1@gmail.com>
2026-09-13 12:24:22 -0400

    Bump printer app for receipt alignment refinement

--- android-print-bridge/app/build.gradle.kts
+++ android-print-bridge/app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.laundryloop.printbridge"
         minSdk = 26
         targetSdk = 35
-        versionCode = 11
-        versionName = "1.10.0"
+        versionCode = 12
+        versionName = "1.11.0"
     }
 
     if (!releaseKeystorePath.isNullOrBlank()) {
