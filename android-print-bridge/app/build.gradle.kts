1d85fdf7b86418c95e53ebc23af32d9ea9e616d9
oscarjacobis1-code <oscarjacobis1@gmail.com>
2026-09-13 13:22:02 -0400

    Bump branded printer app version

--- android-print-bridge/app/build.gradle.kts
+++ android-print-bridge/app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.laundryloop.printbridge"
         minSdk = 26
         targetSdk = 35
-        versionCode = 14
-        versionName = "1.13.0"
+        versionCode = 15
+        versionName = "1.14.0"
     }
 
     if (!releaseKeystorePath.isNullOrBlank()) {
